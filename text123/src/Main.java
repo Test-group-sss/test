feabba94de4471e2b928908c8ef9d09b982006b7
Đỗ Thành Nhân <124018743+thanhnhan3286@users.noreply.github.com>
2023-04-25 16:30:37 +0700

    2

--- text123/src/Main.java
+++ text123/src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("nhan1234");
+        System.out.println(
+                "nhân nhân nhân "
+        );
     }
 
 }
